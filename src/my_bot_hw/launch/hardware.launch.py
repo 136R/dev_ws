709ecb1525f6c3daaad85181bcb16a7ab867fac6
Rpid <K13640213395@outlook.com>
2026-03-18 21:02:33 +0800

    fix: 修复hardware.launch.py executable写法

--- src/my_bot_hw/launch/hardware.launch.py
+++ src/my_bot_hw/launch/hardware.launch.py
@@ -1,25 +1,18 @@
-import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-
     robot_description_content = Command([
-        PathJoinSubstitution([FindExecutable(name='xacro')]),
-        ' ',
+        'xacro ',
         PathJoinSubstitution([FindPackageShare('my_bot'), 'description', 'robot.urdf.xacro']),
-        ' ',
-        'use_ros2_control:=true',
-        ' ',
-        'sim_mode:=false',
-        ' ',
-        'ros2_control_config:=',
+        ' use_ros2_control:=true',
+        ' sim_mode:=false',
+        ' ros2_control_config:=',
         PathJoinSubstitution([FindPackageShare('my_bot_hw'), 'config', 'my_bot_hw.ros2_control.xacro'])
     ])
 
@@ -35,26 +28,26 @@ def generate_launch_description():
         package='controller_manager',
         executable='ros2_control_node',
         parameters=[robot_description, robot_controllers],
-        output='both',
+        output='both'
     )
 
     robot_state_pub_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='both',
-        parameters=[robot_description, {'use_sim_time': use_sim_time}]
+        parameters=[robot_description]
     )
 
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
+        arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager']
     )
 
     robot_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        arguments=['diff_drive_controller', '--controller-manager', '/controller_manager'],
+        arguments=['diff_drive_controller', '--controller-manager', '/controller_manager']
     )
 
     delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
